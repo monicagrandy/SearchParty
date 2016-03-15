2cafab2061d43510ffd7ea8c39ace2d9e93a6d6c
Eleanor Demis <eleanordemis@gmail.com>
2016-03-15 15:58:24 -0700

    [refactor] adds index.js and cors compatability

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 'use strict'
 const express = require('express');
 const app = express();
-const db = require('./db/backup.cypher');
+const methodOverride = require('method-override');
+// const db = require('./db/backup.cypher');rs
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
+app.use(methodOverride());
 
 require('./routes/routes.js')(app, express);
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log('http://localhost:' + port);
-});
+
 
 module.exports = app;
