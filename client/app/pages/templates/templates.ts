2e1c0b80a5a287917ca24b316140763b701887c4
Ethan Rubio <ethanrubio@gmail.com>
2016-03-15 20:41:05 -0700

    [feature] adds test data for templates

--- client/app/pages/templates/templates.ts
+++ client/app/pages/templates/templates.ts
@@ -1,35 +1,63 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
-// import {ItemDetailsPage} from '../item-details/item-details';
+import {TaskPage} from '../tasks/tasks';
 
 
 @Page({
-  templateUrl: 'build/pages/templates/templates.html'
+  templateUrl: 'build/pages/templates/templates.html',
 })
 export class TemplatePage {
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  testData: Array<{type: string, huntname: string, image: string, icon: string}>;
+  items: Array<{title: string, image: string, huntname: string, icon: string}>;
 
   constructor(private nav: NavController, navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
-    // this.selectedItem = navParams.get('item');
+    this.selectedItem = navParams.get('item');
+    
+    this.testData = [
+      {type: 'bar', huntname: 'Bar Hunt', image: 'img/bar.jpg', icon: 'ios-pint'},
+      {type: 'beach', huntname: 'Beach Hunt', image: 'img/beach.jpg', icon: 'ios-water'},
+      {type: 'brunch', huntname: 'Brunch Hunt', image: 'img/brunch.jpg', icon: 'ios-restaurant'},
+      {type: 'coffee', huntname: 'Coffee Hunt', image: 'img/coffee.jpg', icon: 'cafe'},
+      {type: 'park', huntname: 'Park Hunt', image: 'img/park.jpg', icon: 'leaf'},
+      {type: 'ramen', huntname: 'Ramen Hunt', image: 'img/ramen.jpg', icon: 'ios-egg'}
+      ];
 
-    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    // 'american-football', 'boat', 'bluetooth', 'build'];
-
-    // this.items = [];
-    // for(let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
+    this.items = [];
+    for (let hunt of this.testData) {
+      this.items.push({
+        title: hunt.type,
+        image: hunt.image,
+        huntname: hunt.huntname,
+        icon: hunt.icon
+      });
+    }
+  // <ion-card class="adv-map">
+  //   <div style="position: relative">
+  //     <img src="img/ramen.jpg" {{item.image}}>
+  //     <button secondary fab fab-right fab-top class="fab-map">
+  //       <ion-icon name='search'></ion-icon>
+  //     </button>
+  //   </div>
+  //   <ion-item>
+  //     <ion-icon subtle large item-left name='ios-egg' {{item.icon}}></ion-icon>
+  //     <h1>Ramen Hunt{{item.huntname}}</h1>
+  //   </ion-item>
+  //   <ion-item actions>
+  //     <span item-left secondary class="item-bold">26 min</span>
+  //     <span item-left subtle>(8.1 mi)</span>
+  //     <button secondary clear item-right>
+  //       <ion-icon name='navigate'></ion-icon>
+  //       Start
+  //     </button>
+  //   </ion-item>
+  // </ion-card>
   }
 
-  // itemTapped(event, item) {
-  //   this.nav.push(ItemDetailsPage, {
-  //     item: item
-  //   });
-  // }
+  itemTapped(event, item) {
+    console.log(item);
+    this.nav.push(TaskPage, {
+      item: item
+    });
+  }
 }
