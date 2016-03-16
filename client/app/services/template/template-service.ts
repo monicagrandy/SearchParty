65b172a40c3afe75aff52ffbd5f6c3ff993ffca4
Ethan Rubio <ethanrubio@gmail.com>
2016-03-16 12:09:22 -0700

    [feature] basic template service layout

---
+++ client/app/services/template/template-service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from 'angular2/core';
+import {Http, Headers} from 'angular2/http';
+import {ConnectionBackend, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class TemplateService {
+  constructor(private _http:Http) {}
+  getData(){
+    console.log('called get req')
+    let httpGetPromise = new Promise((resolve, reject) => {
+      console.log('called get req')
+      this._http.get('/templates')
+        .map(res => res.json())
+        .subscribe(
+          data => {
+            console.log('data from promise: ', data);
+            resolve(data);
+          },
+          err => reject(err),
+          () => console.log('data recieved')
+          )
+        })
+    return httpGetPromise;
+  }
+
+  postData(data){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    console.log('called get req')
+    let httpGetPromise = new Promise((resolve, reject) => {
+      console.log('called get req')
+      this._http.post('/tasks', data, {headers: headers})
+        .map(res => res.json())
+        .subscribe(
+          data => {
+            console.log('data from promise: ', data);
+            resolve(data);
+          },
+          err => reject(err),
+          () => console.log('data recieved')
+          )
+        })
+    return httpGetPromise;
+  }
+}  
