f01c0558f4c977c2183236e1bb345b5f3e89a29a
Eleanor Demis <eleanordemis@gmail.com>
2016-03-15 19:16:42 -0700

    [refactor] adds callback to sign in cypher query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,43 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const neo = require('../db/neo.js');
 const secret = require('../db/config/config.js')
 
-module.export = {
-  signin: (req, res) => {
-    //parse through the request
-    //extract user information
-    let username = req.body.username;
-    let password = req.body.password;
-    let checkUsernameQuery = //TODO fill in this cypher statement, want to return the entire userObject
-
-    //checking the database to see if the user exists
-    neo.runCypherStatementPromise(checkUsernameQuery).then(
-      //if the user exists
-      let userObject = data[0];
-      if(userObject.username) {
-        //then compare the password
-        bcrypt.compare(password, userObject.password, (err, result) => {
-          if(result) {
-            let token = jwt.encode({username: data[0].username}, secret);
-            res.send(token);
-          } else {
-            res.send("password is incorrect");
-          }
-        })
-      } else {
-        res.send("username does not exist");
-      }
-    )
-    //check the database to see if the user exists
-      //if they do, then we check the password, using bcrypt compare
-        //if it is the correct password
-          //assign a token and send back that token
-        //if it is not the correct password
-          //send back a string that says "incorrect password"
-      //if the user does not exist
-        //send something that tells front end to redirect to sign up
-
-    neo.runCypherStatementPromise(query);
-  },
+module.exports = {
   signup: (req, res) => {
     let password = req.body.password;
     //extract user info from request and assign to some object
@@ -57,8 +21,8 @@ module.export = {
         };
         //adding to the db happens here
         //TODO: Add cypher query syntax
-        let query = `CREATE (${user.userID}:User { ${user} })`
-        neo.runCypherStatementPromise(query);
+        let createUserQuery = `CREATE (${user.userID}:User { ${user} })`
+        neo.runCypherStatementPromise(createUserQuery);
 
         let token = jwt.encode({username: username}, secret);
         res.send(token);
@@ -66,7 +30,37 @@ module.export = {
     })
     //use the props create syntax to pass information to db
   },
-  signout: (req, res) => {
+  signin: (req, res) => {
+    //parse through the request
+    //extract user information
+    let username = req.body.username;
+    let password = req.body.password;
+    let checkUsernameQuery = //TODO fill in this cypher statement, want to return the entire userObject
 
+    //checking the database to see if the user exists
+    neo.runCypherStatementPromise(checkUsernameQuery)
+    .then((err, data) => {
+      //if the user exists
+      let userObject = data[0];
+      if(userObject.username) {
+        //then compare the password
+        bcrypt.compare(password, userObject.password, (err, result) => {
+          //if it is the correct password
+          if(result) {
+            //assign a token and send back that token
+            let token = jwt.encode({username: userObject.username}, secret);
+            res.send(token);
+            //if it is not the correct password
+          } else {
+            //send back a string that says "incorrect password"
+            res.send("password is incorrect");
+          }
+        })
+        //if the user does not exist
+      } else {
+        //send something that tells front end to redirect to sign up
+        res.send("username does not exist");
+      }
+    });
   }
-}
+};
