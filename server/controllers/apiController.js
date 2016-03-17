ac4bad38f03ff459cc69b33af2982e66627896b6
Cameron Roark <cam@cameronroark.com>
2016-03-16 18:31:23 -0700

    [refactor] adjusting yelp api for asynchronous callbacks

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -8,6 +8,7 @@ const apiConfig = require('../db/config/config.js');
 
 module.exports = {
    yelpAPI: (keyword, geolocation, callback) => {
+      const latitude = geolcation.latitude
       randomString (length, chars) => {
          let randStr = '';
          for(var i = length; i > 0; --i) {
@@ -19,19 +20,22 @@ module.exports = {
       let method = 'GET';
       let url = 'http://api.yelp.com/v2/search';
       let params = {
-         location: 'Los+Angeles',
+         // location: 'Los+Angeles',  //Instead of serach by city, we hand it cll=lat, long
+         cll: (geolcation.latitude, geolocation.longitude),
          oauth_consumer_key: apiConfig.yelpConsumerKey,
          oauth_token: apiConfig.yelpToken,
          oauth_signature_method: "HMAC-SHA1",
          oauth_timestamp: new Date().getTime(),
          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-         term: search
+         term: keyword,
+         limit: 10
       };
       let consumerSecret = apiConfig.yelpConsumerSecret;
       let tokenSecret = apiConfig.yelpTokenSecret;
       let signature = oauth_signature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
 
       params['oauth-signature'] = signature;
-      $http.jsonp(url, {params: params}).success(callback);
+      //Returns a list of 10 bars rated closest to current location (according to Yelp);
+      $http.jsonp(url, {params: params}).then(callback);
    }
 };
