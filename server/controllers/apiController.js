92876e8cbf35af0c4e01e0a3a9a34cf2c8f3464a
Cameron Roark <cam@cameronroark.com>
2016-03-16 20:50:49 -0700

    [refactor] yelp api

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -3,39 +3,58 @@
 const oauth = require('oauth');
 const oauthSig = require('oauth-signature');
 const apiConfig = require('../db/config/config.js');
+const request = require('request');
+const qs = require('querystring');
 //Other users will have to go into the config/config.js and insert their own
 //credentials to access the Yelp API.
 
 module.exports = {
-   yelpAPI: (keyword, geolocation, callback) => {
-      const latitude = geolcation.latitude
-      randomString (length, chars) => {
+   yelpAPI: (req, res) => {
+      let keyword = req.body.keyword;
+      let geolocation = req.body.geolocation;
+      let method = 'GET';
+      let url = 'http://api.yelp.com/v2/search';
+      let consumerSecret = "Z1qCGN-gHzLjwkcSYfwlYJG1t_E";
+      let tokenSecret = "oaGzxknyDi80Cu_rSJlDWR1BwSs";
+
+      const randomString = (length, chars) => {
          let randStr = '';
          for(var i = length; i > 0; --i) {
             randStr += chars[Math.round(Math.random() * (chars.length - 1))];
          }
-         console.log('RandomString Generated: ' + randString(32, 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ))
+         // console.log('RandomString Generated: ' + randString(32, 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ))
          return randStr;
       };
-      let method = 'GET';
-      let url = 'http://api.yelp.com/v2/search';
-      let params = {
+
+      let oauth = {
          // location: 'Los+Angeles',  //Instead of serach by city, we hand it cll=lat, long
-         cll: (geolcation.latitude, geolocation.longitude),
-         oauth_consumer_key: apiConfig.yelpConsumerKey,
-         oauth_token: apiConfig.yelpToken,
+         term: keyword,
+         limit: 10,
+         ll: `${geolocation.latitude},${geolocation.longitude}`,
+         oauth_consumer_key: "AgSeqsEmmShlC46CY65RrA",
+         oauth_token: "AnJaHtuYvW22lW_u-K_fMx666UzBO_FF",
          oauth_signature_method: "HMAC-SHA1",
          oauth_timestamp: new Date().getTime(),
          oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-         term: keyword,
-         limit: 10
+         oauth_version: "1.0"
       };
-      let consumerSecret = apiConfig.yelpConsumerSecret;
-      let tokenSecret = apiConfig.yelpTokenSecret;
-      let signature = oauth_signature.generate(method, url, params, consumerSecret, tokenSecret, { encodeSignature: false});
+      let signature = oauthSig.generate(method, url, oauth, consumerSecret, tokenSecret, { encodeSignature: false});
+      oauth.oauth_signature = signature;
+      let paramURL = qs.stringify(oauth);
+      paramURL = paramURL.replace('%2C', ',');
+      let apiURL = url + '?' + paramURL;
+      request({url:apiURL, json:true}, (error, response, body) => {
+         console.log('error:', error);
+         // console.log('body:', body);
+         if(!error && response.statusCode === 200) {
+            // console.log('body:', body);
+            console.log('allgood');
+            res.json({businesses: body.businesses})
+         } else {
+            console.log('error', error);
+            res.status(400).json(error);
+         }
 
-      params['oauth-signature'] = signature;
-      //Returns a list of 10 bars rated closest to current location (according to Yelp);
-      $http.jsonp(url, {params: params}).then(callback);
+      });
    }
 };
