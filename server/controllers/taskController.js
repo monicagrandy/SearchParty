18b88b7392edde13b5709bf518937a7f21e50d92
Eleanor Demis <eleanordemis@gmail.com>
2016-03-22 15:55:49 -0700

    [feature] returns promise from task controller, and updates cypher query

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -5,29 +5,28 @@ const neo = require('../db/neo.js');
 module.exports = {
   getTask: (keyword) => {
 
-    let checkKeywordQuery = `MATCH (t:Task)
-    MATCH (c:Category{title:"${keyword}"})
-    WHERE (c)-[:CONTAINS]->(t)
+    let checkKeywordQuery =
+    `MATCH (c:Category{title:"${keyword}"})-[:CONTAINS]->(t:Task)
     RETURN t ORDER BY rand() LIMIT 10`;
 
-    let getTaskPromise = new Promise((resolve, reject) => {
-      neo.runCypherStatementPromise(checkKeywordQuery)
-      .then((data) => {
-         if(data) {
-            return resolve(data);
-         } else {
-            return reject({error: 'keyword doesnt exist'});
-         }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    });
 
-    return getTaskPromise;
+    return neo.runCypherStatementPromise(checkKeywordQuery)
+    .then((data) => {
+      return new Promise((resolve, reject) => {
+        console.log(data);
+        if(data) {
+          return resolve(data);
+        } else {
+          return reject({error: 'keyword doesnt exist'});
+        }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 }
 
-// CREATE (bar:Keyword {list: ['talk to stranger', 'drink beer in 30 seconds',
-//'take selfie in urinal', 'pick up a guy', 'pick up a girl', 'take a shot',
-//'buy a shot for someone', 'ask a group of strangers to drink beers with you in less than a minute']}) RETURN bar
+  // CREATE (bar:Keyword {list: ['talk to stranger', 'drink beer in 30 seconds',
+  //'take selfie in urinal', 'pick up a guy', 'pick up a girl', 'take a shot',
+  //'buy a shot for someone', 'ask a group of strangers to drink beers with you in less than a minute']}) RETURN bar
