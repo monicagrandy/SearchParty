cf470bb3f5d32e14eca4765e0622944c26add411
Eleanor Demis <eleanordemis@gmail.com>
2016-03-23 15:37:05 -0700

    [bug] fixes syntax error in creating new smaller place object

--- server/lib/hunt/addTasksAndLocationsToHunt.js
+++ server/lib/hunt/addTasksAndLocationsToHunt.js
@@ -9,15 +9,19 @@ module.exports = {
   addTaskAndLocationToHunt: (task, location, huntID) => {
     console.log("inside add tasks and location to hunt");
     let taskID = task.id;
+    // console.log(task.id);
+    // console.log(placeUtil);
+    // console.log("location Object", location);
     let smallerLocation = placeUtil.createSmallerPlaceObject(location);
+    console.log("this is the smaller location", smallerLocation);
 
-    console.log("this is the location data", location.location);
+    // console.log("this is the location data", location);
 
     const insertTaskAndLocationToHuntQuery =
     `MATCH (hunt:Hunt{id:"${huntID}"}), (task:Task{id:${taskID}})
     CREATE (location:Place{name:"${smallerLocation.name}", id:"${smallerLocation.id}", lat:"${smallerLocation.lat}", lng:"${smallerLocation.lng}", address:"${smallerLocation.address}"}})
     CREATE (hunt)-[:INCLUDES]->(task)-[:OCCURRED_AT]->(location)`;
 
-    return neo4jPromise.databaseQueryPromise(insertTaskAndLocationToHuntQuery, locationProps);
+    return neo4jPromise.databaseQueryPromise(insertTaskAndLocationToHuntQuery);
   }
 }
--- server/lib/util/buildPlaceInformation.js
+++ server/lib/util/buildPlaceInformation.js
@@ -2,13 +2,19 @@
 
 module.exports = {
   createSmallerPlaceObject: placeObj => {
-    let smallerPlace = {
-      name: placeObj.name,
-      id: placeObj.id,
-      lat: placeObj.coordinate.latitude,
-      lng: placeObj.coordinate.longitude,
-      address: placeObj.location.display_address[0] + ', ' + placeObj.location.display_address[2];
-    };
-    return smallerPlace;
+    console.log("inside of the smaller object builder");
+    let smallerObj = {};
+      smallerObj.name = placeObj.name;
+      smallerObj.id = placeObj.id;
+      smallerObj.lat = placeObj.location.coordinate.latitude;
+      smallerObj.lng = placeObj.location.coordinate.longitude;
+      smallerObj.address = placeObj.location.display_address[0] + ', ' + placeObj.location.display_address[2];
+      //
+      // let smallerObj = {
+      //   name: placeObj.name
+      // }
+
+    console.log("inside of smaller object builder (small)", smallerObj);
+    return smallerObj;
   }
 }
