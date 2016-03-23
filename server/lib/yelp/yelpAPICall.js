77d1f70cb0be0a2b8a760d0bcdc40a9fc8a20961
Eleanor Demis <eleanordemis@gmail.com>
2016-03-22 19:04:56 -0700

    [refactor] adds yelp api call promise

--- server/lib/yelp/yelpAPICall.js
+++ server/lib/yelp/yelpAPICall.js
@@ -1,9 +1,21 @@
 'use strict'
 
 const config = require('../config/config.js');
+const oAuth = require('./yelpOauthGenerator.js');
 
 module.exports = {
   yelpPlacesBasedOnKeyword: (keyword, geolocation) => {
+    let apiUrl = oAuth.generateOauthCredentials(keyword, geolocation);
 
+    return new Promise((resolve, reject) => {
+      request({url:apiURL, json:true}, (error, response) => {
+        console.log('error: ', error);
+        if(!error && response.statusCode === 200){
+          return resolve(response.body);
+        } else {
+          return reject({error: error});
+        }
+      })
+    })    
   }
 }
