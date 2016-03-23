511e28c58c41b799a974f0ee0189e141c40a0de2
Eleanor Demis <eleanordemis@gmail.com>
2016-03-22 17:31:54 -0700

    [feature] builds out skeleton of adding tasks and locations to existing hunt

--- server/lib/hunt/addTasksAndLocationsToHunt.js
+++ server/lib/hunt/addTasksAndLocationsToHunt.js
@@ -6,10 +6,16 @@ const neo4jPromise = require('../neo4j/neo4jQueryPromiseReturn.js');
 module.exports = {
 
   addTaskAndLocationToHunt: (task, location, huntID) => {
-
+    let taskID = task.id;
+    let locationProps = {
+      "props": location;
+    };
+    
     const insertTaskAndLocationToHuntQuery =
-    ``;
+    `MATCH (hunt:Hunt{id:${huntID}}), (task:Task{id:${taskID}})
+    CREATE (location:Place{props})
+    CREATE (hunt)-[:INCLUDES]->(task)-[:OCCURRED_AT]->(location)`;
 
-    return neo4jPromise.databaseQueryPromise(insertTaskAndLocationToHuntQuery);
+    return neo4jPromise.databaseQueryPromise(insertTaskAndLocationToHuntQuery, locationProps);
   }
 }
--- server/lib/neo4j/neo4jQueryPromiseReturn.js
+++ server/lib/neo4j/neo4jQueryPromiseReturn.js
@@ -4,8 +4,8 @@ const neo = require('../db/neo.js');
 
 module.exports = {
 
-  databaseQueryPromise: query => {
-    return neo.runCypherStatementPromise(query)
+  databaseQueryPromise: (query, properties) => {
+    return neo.runCypherStatementPromise(query, properties)
     .then(data => {
       return new Promise((resolve, reject) => {
         if(data) {
