6afed772a2c0f90c28e4b55b28e123d142867f79
Ethan Rubio <ethanrubio@gmail.com>
2016-03-24 14:00:23 -0700

    [chore] adds necessary semi-colons

--- client/app/pages/tasks/tasks.ts
+++ client/app/pages/tasks/tasks.ts
@@ -91,10 +91,10 @@ export class TaskPage {
       this._taskService.postData(JSON.stringify(dataObj), this.TASKS_URL)
         .then(result => {
           this.locName = result.businesses.name;
-          this.currChallenge = result.tasks.content
-          this.previousPlaces.push(result.businesses)
+          this.currChallenge = result.tasks.content;
+          this.previousPlaces.push(result.businesses);
           this.locAddress = result.businesses.location.display_address[0] + ', ' + result.businesses.location.display_address[2];
-          this.previousTasks.push(result.tasks)
+          this.previousTasks.push(result.tasks);
           this.locLat = result.businesses.location.coordinate.latitude;
           this.locLng = result.businesses.location.coordinate.longitude;
           this.markComplete();
