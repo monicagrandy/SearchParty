9573a036d974acf15ae7383427981c69485af65d
Cameron Roark <cam@cameronroark.com>
2016-03-26 15:03:12 -0700

    [chore] creates database query for retrieving chat system

---
+++ server/lib/user/retrieveChat.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const neo = require('../../db/neo.js');
+const neo4jPromise = require('../neo4j/neo4jQueryPromiseReturn.js');
+
+module.exports = {
+   retrieveChat: (huntID) => {
+
+      const retrieveChatQuery =
+      `MATCH (hunt:Hunt{huntID:"h4J3JLp0pg"})-[:HAS_CHAT]->(chat)
+       RETURN chat`;
+
+      return neo4jPromise.databaseQueryPromise(retrieveChatQuery);
+   }
+
+}
