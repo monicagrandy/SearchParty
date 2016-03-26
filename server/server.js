197bd500067d9d4395bbc66676acac14aeac7e85
Ethan Rubio <ethanrubio@gmail.com>
2016-03-25 22:42:00 -0700

    [chore] adds certifcates for ssl

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const methodOverride = require('method-override');
 // const db = require('./db/backup.cypher');rs
 
+
 const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
