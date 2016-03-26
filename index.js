460a115f94aa3b8287dd759a6ad26b4955bbfe6e
Ethan Rubio <ethanrubio@gmail.com>
2016-03-26 10:50:57 -0700

    [refactor] properly creates http/https servers based on required ports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 const app = require('./server/server.js');
-// const port = 80;
+const config = require('./server/config/config.js');
+const httpPort = config.HTTPPORT || 8000;
+const httpsPort = config.HTTPSPORT || 8001;
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
@@ -10,11 +12,7 @@ const options = {
   cert: fs.readFileSync('server.crt')
 };
 
-// app.listen(port, () => {
-//   console.log(`Listening to ${port}`);
-// });
-
 // Create an HTTP service.
-http.createServer(app).listen(80);
+http.createServer(app).listen(httpPort, () => console.log(`express HTTP server listening on port ${httpPort}`));
 // Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(443, () => console.log('Https server listening on port 443') );
+https.createServer(options, app).listen(httpsPort, () => console.log(`express HTTPS server listening on port ${httpsPort}`) );
