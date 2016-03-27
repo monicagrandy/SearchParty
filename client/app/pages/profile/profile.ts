cbdfbf9f6b6c8cb5bc3ffcd65f8eac029bcd2296
Ethan Rubio <ethanrubio@gmail.com>
2016-03-26 17:03:34 -0700

    [feature] adds add friend functionality

--- client/app/pages/profile/profile.ts
+++ client/app/pages/profile/profile.ts
@@ -1,25 +1,33 @@
 import {Page, NavController, NavParams, LocalStorage, Storage} from 'ionic-angular';
 import {ProfileService} from '../../services/profile/profile-service';
+import {FriendService} from '../../services/friend/friend-service';
 import {AuthService} from '../../services/auth/auth-service';
 import {FORM_DIRECTIVES} from 'angular2/common';
 import {PastHuntsPage} from '../past-hunts/past-hunts';
 
 @Page({
   templateUrl: 'build/pages/profile/profile.html',
-  providers: [ProfileService],
+  providers: [ProfileService, FriendService],
   directives: [FORM_DIRECTIVES]
 })
 export class ProfilePage {
   local: Storage = new Storage(LocalStorage);
   hunts: any;
+  token: any;
   // sample types for hunts and friends
   // friends: Array<{username: string, profile_image: string}>;
   // hunts: Array<{type: string, huntname: string, image: string, icon: string}>;
 
-  constructor(private nav: NavController, navParams: NavParams, private profileService: ProfileService, private auth: AuthService) {
-    let token = this.local.get('id_token')._result;
-    console.log('this is the token before it is sent', token);
-    this.profileService.getProfile(token)
+  constructor(
+    private nav: NavController, 
+    navParams: NavParams,
+    private profileService: ProfileService,
+    private auth: AuthService,
+    private friendService: FriendService
+    ) {
+    this.token = this.local.get('id_token')._result;
+    console.log('this is the token before it is sent', this.token);
+    this.profileService.getProfile(this.token)
       .then(data => {
         console.log(data.hunts);
         // this.friends = data.friends;
@@ -41,5 +49,10 @@ export class ProfilePage {
   
   addFriend(friend) {
     console.log(friend);
+    this.friendService.addFriend(this.token, friend)
+      .then(data => {
+        console.log('friend added! ', data);
+      })
+        .catch(error => console.log(error));
   }
 }
---
+++ client/app/services/friend/friend-service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from 'angular2/core';
+import {Http, Headers} from 'angular2/http';
+import {ConnectionBackend, HTTP_PROVIDERS} from 'angular2/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class FriendService {
+  ADDFRIEND_URL: string = 'http://localhost:8000/addFriend'; //update this later https://getsearchparty.com/addFriend
+  GETFRIENDS_URL: string = 'http://localhost:8000/getFriends'; //update this later https://getsearchparty.com/getFriends
+  contentHeader: Headers = new Headers({'Content-Type': 'application/json'});
+  
+  constructor(private _http:Http) {}
+  
+  addFriend(userToken, friend) {
+    console.log('called post req');
+    let dataToSend = { token: userToken, friendusername: friend.username };
+    
+    let addFriendPostPromise = new Promise((resolve, reject) => {
+      
+      this._http.post(this.ADDFRIEND_URL, JSON.stringify(dataToSend), {headers: this.contentHeader})
+        .map(res => res.json())
+        .subscribe(
+            data => {
+              console.log('data from promise: ', data);
+              resolve(data);
+          },
+            err => {
+              this.logError(err);
+              reject(err);
+          },
+            () => console.log('data recieved')
+          )
+    });
+    
+    return addFriendPostPromise;
+  }
+  
+  getFriends(userToken) {
+    
+  }
+  
+  logError(err) {
+    console.error('There was an error: ' + err);
+  }
+}  
