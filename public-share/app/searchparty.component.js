2478cfee63f105dca172302e84c07e3658443ee3
Ethan Rubio <ethanrubio@gmail.com>
2016-03-28 14:52:25 -0700

    [chore] removes unnecessary imports

--- public-share/app/searchparty.component.js
+++ public-share/app/searchparty.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-material/all', 'rxjs/add/operator/map'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', 'ng2-material/all', './searchparty.service', 'rxjs/add/operator/map'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,15 +10,12 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1, router_1, all_1;
+    var core_1, router_1, all_1, searchparty_service_1;
     var SearchPartyComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (http_1_1) {
-                http_1 = http_1_1;
             },
             function (router_1_1) {
                 router_1 = router_1_1;
@@ -26,14 +23,19 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
             function (all_1_1) {
                 all_1 = all_1_1;
             },
+            function (searchparty_service_1_1) {
+                searchparty_service_1 = searchparty_service_1_1;
+            },
             function (_1) {}],
         execute: function() {
             SearchPartyComponent = (function () {
-                function SearchPartyComponent(_http, _params) {
-                    this._http = _http;
+                function SearchPartyComponent(_params, _searchPartyService) {
                     this._params = _params;
-                    this._http = _http;
+                    this._searchPartyService = _searchPartyService;
                     this.huntID = _params.get('huntID');
+                    this._searchPartyService.getHunt(this.huntID)
+                        .then(function (data) { return console.log(data); })
+                        .catch(function (err) { return console.log(err); });
                 }
                 SearchPartyComponent = __decorate([
                     core_1.Component({
@@ -41,14 +43,9 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
                         templateUrl: './share/app/searchparty.component.html',
                         styleUrls: ['./share/app/searchparty.component.css'],
                         directives: [router_1.ROUTER_DIRECTIVES, all_1.MATERIAL_DIRECTIVES],
-                        providers: [
-                            http_1.Http,
-                            http_1.ConnectionBackend,
-                            http_1.HTTP_PROVIDERS,
-                            all_1.MATERIAL_PROVIDERS
-                        ]
+                        providers: [all_1.MATERIAL_PROVIDERS, searchparty_service_1.SearchPartyService]
                     }), 
-                    __metadata('design:paramtypes', [http_1.Http, router_1.RouteParams])
+                    __metadata('design:paramtypes', [router_1.RouteParams, searchparty_service_1.SearchPartyService])
                 ], SearchPartyComponent);
                 return SearchPartyComponent;
             }());
--- public-share/app/searchparty.component.ts
+++ public-share/app/searchparty.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
-import {Http, ConnectionBackend, HTTP_PROVIDERS, Headers} from 'angular2/http';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteParams} from 'angular2/router';
 import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material/all';
+import {SearchPartyService} from './searchparty.service';
 import 'rxjs/add/operator/map';
 import * as _ from 'underscore';
 
@@ -10,20 +10,18 @@ import * as _ from 'underscore';
   templateUrl: './share/app/searchparty.component.html',
   styleUrls: ['./share/app/searchparty.component.css'],
   directives: [ROUTER_DIRECTIVES, MATERIAL_DIRECTIVES],
-  providers: [
-    Http,
-    ConnectionBackend,
-    HTTP_PROVIDERS,
-    MATERIAL_PROVIDERS
-  ]
+  providers: [MATERIAL_PROVIDERS, SearchPartyService]
 })
 export class SearchPartyComponent {
   
   huntID: any;
+  error: any;
   
-  constructor(private _http:Http, private _params: RouteParams) {
-    this._http = _http;
+  constructor(private _params: RouteParams, private _searchPartyService: SearchPartyService) {
     this.huntID = _params.get('huntID');
+    this._searchPartyService.getHunt(this.huntID)
+      .then(data => console.log(data))
+        .catch(err => console.log(err));
   }
 
 }
