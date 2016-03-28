c9d4e3860db249f758e1a10bcc9c4d10e8b17f2f
Ethan Rubio <ethanrubio@gmail.com>
2016-03-28 11:37:02 -0700

    [chore] removes unnecessary committed files

--- public-share/typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/google.maps/index.d.ts" />
-/// <reference path="browser/ambient/jasmine/index.d.ts" />
-/// <reference path="browser/ambient/underscore/index.d.ts" />
