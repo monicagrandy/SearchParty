583d84350cea385cf5aa77e0d6875f4021b2cf9e
Eleanor Demis <eleanordemis@gmail.com>
2016-03-29 12:11:08 -0700

    [feature] fixes cypher query for entire user hunt info

--- server/lib/user/showUserHuntsAndInfo.js
+++ server/lib/user/showUserHuntsAndInfo.js
@@ -5,14 +5,21 @@ const userFormat = require('../util/formatUserObject.js');
 module.exports = {
   giveAllUserHuntData: username => {
     console.log("inside of give all user hunt data function");
-    const returnAllUserInfoQuery = `MATCH (user:User{username:"${username}"})-[*]->(node) RETURN node`;
+    const returnAllUserInfoQuery = `MATCH (user:User{username:"${username}"})-[:PARTICIPATED_IN]-(hunt)
+    WITH COLLECT(DISTINCT hunt) AS hunts
+    UNWIND hunts AS h
+    WITH h
+    MATCH (h)-[:OCCURRED_AT*]->(place)-[:INCLUDES*]->(task)
+    OPTIONAL MATCH (h)-[:HAS_CHAT*]->(chatInfo)-[*]->(message)
+    WITH COLLECT(DISTINCT task) AS tasks, COLLECT(DISTINCT place) AS places, COLLECT(DISTINCT message) AS messages, h, chatInfo
+    RETURN {places: places, tasks: tasks, messages: messages, huntData: h, chatData: chatInfo}`;
 
     return neo4jPromise.databaseQueryPromise(returnAllUserInfoQuery)
     .then(allUserData => {
       console.log("++All userData", allUserData);
-      var prettyUserObject = userFormat.createPrettyUserObject(allUserData);
+      // var prettyUserObject = userFormat.createPrettyUserObject(allUserData);
       return new Promise((resolve, reject) => {
-        resolve(prettyUserObject);
+        resolve(allUserData);
         reject({error: "cannot return user"});
       })
     })
