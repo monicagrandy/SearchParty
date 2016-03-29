45b8783c0c061e24bd2815c482b331befc5df4a2
Cameron Roark <cam@cameronroark.com>
2016-03-29 15:23:02 -0700

    [chore] starts to show when users are typing in chat

--- client/app/pages/chat/chat.ts
+++ client/app/pages/chat/chat.ts
@@ -14,13 +14,17 @@ export class Chat {
    socket: any;
    zone: any;
    chatBox: any;
-   
+   io: any;
+   username: any;
+   timeout: any;
+   timoutFunction: any;
+
    constructor(
       private http: Http,
       private nav: NavController,
       private navParams: NavParams
    ) {
-     let socket = io();
+     let socket = this.io();
      this.messages = [];
      this.zone = new NgZone({enableLongStackTrace: false});
      this.chatBox = "";
@@ -30,12 +34,74 @@ export class Chat {
          this.messages.push(msg);
        });
      });
-   }
-   
+
+     //See when someone is typing:
+     var typing = false;
+     var timeout = undefined;
+
+      function timeoutFunction() {
+        typing = false;
+        socket.emit("typing", false);
+      }
+
+      // $("#msg").keypress(function(e){
+      //   if (e.which !== 13) {
+      //     if (typing === false && myRoomID !== null && $("#msg").is(":focus")) {
+      //       typing = true;
+      //       socket.emit("typing", true);
+      //     } else {
+      //       clearTimeout(timeout);
+      //       timeout = setTimeout(timeoutFunction, 5000);
+      //     }
+      //   }
+      // });
+      //:::UPON USER TYPING, SEND TYPING MESSAGE TO SERVER:::
+      sendTyping(characters) => {
+         if(characters > 0) {
+            typing = true;
+            socket.emit("typing", true);
+         } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(timeoutFunction, 5000);
+         }
+      }
+
+      // socket.on("isTyping", function(data) {
+      //   if (data.isTyping) {
+      //     if ($("#"+data.person+"").length === 0) {
+      //       $("#updates").append("<li id='"+ data.person +"'><span class='text-muted'><small><i class='fa fa-keyboard-o'></i>" + data.person + " is typing.</small></li>");
+      //       timeout = setTimeout(timeoutFunction, 5000);
+      //     }
+      //   } else {
+      //     $("#"+data.person+"").remove();
+      //   }
+      // });
+      //:::UPON USER RECEIVING isTyping FROM SERVER, DISPLAY IT:::
+      socket.on("isTyping", function(data) {
+         console.log('Data from server about typing: ', data);
+       if (data.isTyping) {
+          timeout = setTimeout(timeoutFunction, 5000);
+          //Append ionic username div
+       } else {
+          //Remove ionic username div
+       }
+      });
+
+
+      socket.on("chat", function(person, msg) {
+        $("#msgs").append("<li><strong><span class='text-success'>" + person.name + "</span></strong>: " + msg + "</li>");
+        //clear typing field
+         $("#"+person.name+"").remove();
+         clearTimeout(timeout);
+         timeout = setTimeout(timeoutFunction, 0);
+      });
+}
+
+
   send(message) {
     if (message && message != "") {
-      this.socket.emit("chat_message", message);
+      this.socket.emit("chat_message", message, this.username);
     }
     this.chatBox = "";
-  }   
+  }
 }
