2faa9c9fa1824b2fc64ed1ce7ed6b937223aaf1d
Monica Grandy <monica.grandy@gmail.com>
2016-03-30 19:50:46 -0700

    [bug] makes link highltable

--- client/typings/browser.d.ts
+++ client/typings/browser.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
 /// <reference path="browser/ambient/google.maps/index.d.ts" />
 /// <reference path="browser/ambient/moment/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
