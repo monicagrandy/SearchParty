5176c1a6a4860cc1c07fd084ab1a1646c90ac9af
Ethan Rubio <ethanrubio@gmail.com>
2016-03-31 20:27:35 -0700

    [bug] fixes friends able to join other friends hunts

--- server/lib/user/friends.js
+++ server/lib/user/friends.js
@@ -1,5 +1,6 @@
 'use strict'
 const neo4jPromise = require('../neo4j/neo4jQueryPromiseReturn.js');
+const userFormat = require('../util/formatUserObject.js');
 
 module.exports = {
   addFriendToDBPromise: (mainFriendUsername, friendToBeAddedUsername) => {
@@ -59,21 +60,50 @@ module.exports = {
     }).catch(error => console.error(error));
   }, 
 
-  retrieveAddedHuntsPromise: (username) => {
-    let addedHuntsQuery =
+  // retrieveAddedHuntsPromise: (username) => {
+  //   let addedHuntsQuery =
+  //   `MATCH (user:User{username:"${username}"})-[:ADDED_TO]->(hunt)
+  //   WITH COLLECT (distinct hunt) as hunts
+  //   RETURN hunts`;
+  //   return neo4jPromise.databaseQueryPromise(addedHuntsQuery)
+  //   .then(huntArray => {
+  //     return new Promise((resolve, reject) => {
+  //       if(huntArray !== null) {
+  //         console.log('+++71 this is the retrieveAddedHuntsPromise data ', huntArray);
+  //         resolve(huntArray);
+  //       } else {
+  //         reject({"error": "error finding hunts"});
+  //       }
+  //     })
+  //   }).catch(error => console.error(error));
+  // }
+  
+  retrieveAddedHuntsPromise: username => {
+    let addedHuntsQuery = 
     `MATCH (user:User{username:"${username}"})-[:ADDED_TO]->(hunt)
     WITH COLLECT (distinct hunt) as hunts
-    RETURN hunts`;
+    UNWIND hunts AS h
+    WITH h
+    MATCH (h)-[:OCCURRED_AT*]->(place)-[:INCLUDES*]->(task)
+    MATCH (h)-[:HAS_CHAT*]->(chatInfo)
+    OPTIONAL MATCH (chatInfo)-[*]->(message)
+    OPTIONAL MATCH (h)-[:HAS_PIC*]->(picurl)
+    OPTIONAL MATCH (h)<-[:ABOUT]-(feedback)
+    WITH COLLECT(DISTINCT task) AS tasks, COLLECT(DISTINCT place) AS places, COLLECT(DISTINCT message) AS messages, COLLECT(DISTINCT picurl) AS urls, h, chatInfo, feedback
+    RETURN {places: places, tasks: tasks, urls: urls, messages: messages, huntData: h, chatData: chatInfo, feedback: feedback}
+    `;
     return neo4jPromise.databaseQueryPromise(addedHuntsQuery)
-    .then(huntArray => {
-      return new Promise((resolve, reject) => {
-        if(huntArray !== null) {
-          console.log('+++71 this is the retrieveAddedHuntsPromise data ', huntArray);
-          resolve(huntArray);
-        } else {
-          reject({"error": "error finding hunts"});
-        }
+      .then(huntArray => {
+        return new Promise((resolve, reject) => {
+          if (huntArray !== null) {
+            var prettyUserObject = userFormat.createPrettyUserObject(huntArray);
+            console.log('+++line100 this is the retrieveAddedHuntsPromise data ', prettyUserObject);
+            resolve(prettyUserObject);
+          } else {
+            reject({"error": "error finding hunts"});
+          }
+        })
       })
-    }).catch(error => console.error(error));
+        .catch(error => console.error(error));
   }
 }
