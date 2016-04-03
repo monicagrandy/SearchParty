e8e5f91786c41e91f6fc2b7e62fc050d11a15e07
Ethan Rubio <ethanrubio@gmail.com>
2016-04-03 16:05:24 -0700

    [refactor] refactors socket chat on public-share

--- public-share/app/app.component.js
+++ public-share/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-material/all', './searchparty.component'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-material/all', './searchparty.component', './url-service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1, router_1, all_1, searchparty_component_1;
+    var core_1, http_1, router_1, all_1, searchparty_component_1, url_service_1;
     var AppComponent;
     return {
         setters:[
@@ -28,12 +28,22 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
             },
             function (searchparty_component_1_1) {
                 searchparty_component_1 = searchparty_component_1_1;
+            },
+            function (url_service_1_1) {
+                url_service_1 = url_service_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent() {
+                function AppComponent(urlService) {
+                    this.urlService = urlService;
                     this.title = 'Search Party';
-                    // _.each([1,2,3,], number => console.log(number));
+                    // comment urlService for deployment
+                    this.urlService.grabUrls()
+                        .then(function (urls) {
+                        for (var key in urls) {
+                            localStorage.setItem(key, urls[key]);
+                        }
+                    });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
@@ -45,7 +55,8 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
                             http_1.Http,
                             http_1.ConnectionBackend,
                             http_1.HTTP_PROVIDERS,
-                            all_1.MATERIAL_PROVIDERS
+                            all_1.MATERIAL_PROVIDERS,
+                            url_service_1.UrlService
                         ]
                     }),
                     router_1.RouteConfig([
@@ -55,7 +66,7 @@ System.register(['angular2/core', 'angular2/http', 'angular2/router', 'ng2-mater
                             component: searchparty_component_1.SearchPartyComponent
                         },
                     ]), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [url_service_1.UrlService])
                 ], AppComponent);
                 return AppComponent;
             }());
--- public-share/app/chat.component.js
+++ public-share/app/chat.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'ng2-material/all', 'angular2/router', './chat.service', 'moment'], function(exports_1, context_1) {
+System.register(['angular2/core', 'ng2-material/all', 'angular2/router', './chat.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,13 +10,12 @@ System.register(['angular2/core', 'ng2-material/all', 'angular2/router', './chat
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, all_1, router_1, chat_service_1, core_2, moment;
+    var core_1, all_1, router_1, chat_service_1;
     var ChatComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-                core_2 = core_1_1;
             },
             function (all_1_1) {
                 all_1 = all_1_1;
@@ -26,104 +25,46 @@ System.register(['angular2/core', 'ng2-material/all', 'angular2/router', './chat
             },
             function (chat_service_1_1) {
                 chat_service_1 = chat_service_1_1;
-            },
-            function (moment_1) {
-                moment = moment_1;
             }],
         execute: function() {
             ChatComponent = (function () {
-                function ChatComponent(_chatService, _params, dialog, element) {
+                function ChatComponent(navParams, _chatService) {
                     var _this = this;
                     this._chatService = _chatService;
-                    this._params = _params;
-                    this.dialog = dialog;
-                    this.element = element;
                     this.nameAdded = false;
-                    this.ADD_MESSAGE_URL = 'https://getsearchparty.com/addChatMessage';
-                    this.GET_MESSAGES_URL = 'https://getsearchparty.com/getChatMessages';
-                    this.huntID = _params.get('huntID');
-                    var socket = io.connect('http://localhost:8000');
-                    this.otherUserTyping = false;
-                    this.otherUsername = '';
+                    this.huntID = navParams.get('huntID');
                     this.messages = [];
-                    this.timeout;
-                    this.zone = new core_2.NgZone({ enableLongStackTrace: false });
-                    this.chatBox = "";
-                    this.socket = socket;
-                    this.socket.on("connect", function () {
-                        _this.socket.emit('huntChatRoom', _this.huntID);
-                    });
-                    this.socket.on("chat_message", function (msg, username, datetime) {
-                        _this.zone.run(function () {
-                            console.log(_this.messages);
-                            datetime = moment.unix(datetime).fromNow();
-                            _this.messages.push([username, msg, datetime]);
-                        });
-                    });
-                    this.socket.on("isTyping", function (bool, username) {
-                        if (bool === true && username !== _this.username) {
-                            _this.otherUsername = username;
-                            _this.otherUserTyping = true;
-                        }
-                        else {
-                            _this.otherUserTyping = false;
-                        }
-                    });
-                    var huntIDObject = { huntID: this.huntID };
-                    this._chatService.postData(JSON.stringify(huntIDObject), this.GET_MESSAGES_URL)
-                        .then(function (messagesFromDB) {
-                        _this.zone.run(function () {
-                            console.log("messages from DB", messagesFromDB);
-                            var messagesArray = messagesFromDB.chatMessages;
-                            for (var i = 0; i < messagesArray.length; i++) {
-                                var datetime = moment.unix(messagesArray[i].datetime).fromNow();
-                                console.log('THIS IS BEING PUSHED TO MESSAGES ARRAY');
-                                console.log(messagesArray[i].username, messagesArray[i].text, datetime);
-                                _this.messages.push([messagesArray[i].username, messagesArray[i].text, datetime]);
-                            }
-                        });
-                    }).catch(function (error) { return console.error(error); });
-                    //Change this later
-                    // this.username = window.prompt('Enter a username!', '');
+                    this.chatBox = '';
+                    // watch for messages
+                    // this._chatService.messageChange.subscribe(messages => console.log('this is the change emitted messages ', messages));
+                    // watch for typing
+                    this._chatService.otherUsernameChange.subscribe(function (otherUsername) { return _this.otherUsername = otherUsername; });
+                    this._chatService.otherUserTypingChange.subscribe(function (otherUserTyping) { return _this.otherUserTyping = otherUserTyping; });
                 }
                 ChatComponent.prototype.getUsername = function (username) {
+                    var _this = this;
                     this.username = username;
                     this.nameAdded = true;
-                };
-                ChatComponent.prototype.invocation = function () {
-                    var _this = this;
-                    this.timeout = setTimeout(function () {
-                        _this.socket.emit('typing', false, _this.username, _this.huntID);
-                    }, 1000);
+                    // create socket
+                    this._chatService.createSocket(this.huntID, this.username);
+                    this._chatService.getMessages()
+                        .then(function (messages) {
+                        console.log('this is the data from _chatService getmessages ', messages);
+                        _this.messages = messages;
+                    })
+                        .catch(function (error) { return console.log('there was an error ', error); });
                 };
                 ChatComponent.prototype.OnKey = function (event) {
                     if (event) {
-                        this.socket.emit('typing', true, this.username, this.huntID);
-                        clearTimeout(this.timeout);
-                        this.invocation();
+                        this._chatService.userIsTyping();
                     }
                 };
                 ;
                 ChatComponent.prototype.send = function (message) {
-                    var _this = this;
-                    if (message && message !== "") {
-                        clearTimeout(this.timeout);
-                        this.socket.emit('typing', false, this.username, this.huntID);
-                        var messageObject = {
-                            username: this.username,
-                            huntID: this.huntID,
-                            message: message
-                        };
-                        this._chatService.postData(JSON.stringify(messageObject), this.ADD_MESSAGE_URL)
-                            .then(function (messageAdded) {
-                            messageAdded = messageAdded[0];
-                            console.log("message  added", messageAdded);
-                            _this.socket.emit("chat_message", messageAdded.text, messageAdded.username, messageAdded.datetime, _this.huntID);
-                        }).catch(function (error) {
-                            console.error(error);
-                        });
+                    if (message && message !== '') {
+                        this._chatService.send(message);
                     }
-                    this.chatBox = "";
+                    this.chatBox = '';
                 };
                 ChatComponent = __decorate([
                     core_1.Component({
@@ -133,7 +74,7 @@ System.register(['angular2/core', 'ng2-material/all', 'angular2/router', './chat
                         directives: [all_1.MATERIAL_DIRECTIVES],
                         providers: [all_1.MATERIAL_PROVIDERS, chat_service_1.ChatService]
                     }), 
-                    __metadata('design:paramtypes', [chat_service_1.ChatService, router_1.RouteParams, all_1.MdDialog, core_1.ElementRef])
+                    __metadata('design:paramtypes', [router_1.RouteParams, chat_service_1.ChatService])
                 ], ChatComponent);
                 return ChatComponent;
             }());
